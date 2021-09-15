607d6007c1d2a21ed97a9b8a8649e3f4df99e0ad
wuwei2Y <601820676@qq.com>
2021-09-15 08:59:28 +0800

    基础项目搭建完毕

--- src/main/java/com/looca/service/TestService.java
+++ src/main/java/com/looca/service/TestService.java
@@ -1,9 +1,12 @@
 package com.looca.service;
 
+import com.spring.annotation.Component;
+
 /**
  * 测试用例service
  * @author looca
  */
+@Component
 public class TestService {
 
     public void test() {
