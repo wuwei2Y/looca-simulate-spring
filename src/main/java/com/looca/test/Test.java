456b27af8393beddee5d597756ced529bb025d58
wuwei2Y <601820676@qq.com>
2021-09-15 09:12:59 +0800

    基础项目搭建完毕

--- src/main/java/com/looca/test/Test.java
+++ src/main/java/com/looca/test/Test.java
@@ -12,6 +12,10 @@ public class Test {
 
     public static void main(String[] args) {
 
+        /**
+         * 1. 扫描bean
+         * 2. 创建单例bean(忽略懒加载的情况)
+         */
         LoocaApplicationContext applicationContext = new LoocaApplicationContext(AppConfig.class);
         TestService testService = (TestService) applicationContext.getBean("testService");
 
